9cb04349af1904af1b408fc83ed357f9b673daa5
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-08-16 12:46:23 -0700

    Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato Tyler wiltz is a potato

---
+++ src/yes.java
@@ -0,0 +1,126 @@
+import javax.swing.JOptionPane;
+
+public class yes {
+public static void main(String[] args) {
+	
+	String q=JOptionPane.showInputDialog("chose a color ");
+	
+	if (q.equals("red")) {
+	JOptionPane.showMessageDialog(null, "caprisun will now be your toilet");
+	}
+	
+	if (q.equals("blue")) {
+		JOptionPane.showMessageDialog(null, "chuck e cheese");
+		}
+	
+	if (q.equals("green")) {
+		JOptionPane.showMessageDialog(null, "tomas the train is watching you");
+		}
+	
+	if (q.equals("orange")) {
+		JOptionPane.showMessageDialog(null, "chinckn nuggets");
+		}
+	
+	if (q.equals("yellow")) {
+		JOptionPane.showMessageDialog(null, "GO TO OBAMA.COM FOR FREE ROBUX");
+		}
+		
+	if (q.equals("purple")) {
+		JOptionPane.showMessageDialog(null, "no u");
+		}
+		
+	if (q.equals("black")) {
+		JOptionPane.showMessageDialog(null, "head sholder knees and toes knees and toes");
+		}
+		
+	if (q.equals("pink")) {
+		JOptionPane.showMessageDialog(null, "you are a gummy bear, you are a gummy bear");
+		}
+	
+	if (q.equals("brown")) {
+		JOptionPane.showMessageDialog(null, "your head is made out of poop");
+		}
+	
+	if (q.equals("white")) {
+		JOptionPane.showMessageDialog(null, "a unicorn will fall on you");
+		}
+	
+	if (q.equals("grey")) {
+		JOptionPane.showMessageDialog(null, "Tyler wiltz is a potato");
+		}
+	
+	if (q.equals("light red")) {
+	JOptionPane.showMessageDialog(null, "what do you want");
+	}
+	
+	if (q.equals("light blue")) {
+		JOptionPane.showMessageDialog(null, "barny is a pin dinsour");
+		}
+	
+	if (q.equals("light green")) {
+		JOptionPane.showMessageDialog(null, "tomato");
+		}
+	
+	if (q.equals("light orange")) {
+		JOptionPane.showMessageDialog(null, ":O");
+		}
+	
+	if (q.equals("light yellow")) {
+		JOptionPane.showMessageDialog(null, "1 + 1 = 3");
+		}
+		
+	if (q.equals("light purple")) {
+		JOptionPane.showMessageDialog(null, "ok");
+		}
+		
+	if (q.equals("light pink")) {
+		JOptionPane.showMessageDialog(null, "cris cros apple sause");
+		}
+	
+	if (q.equals("light brown")) {
+		JOptionPane.showMessageDialog(null, "Tyler can poop");
+		}
+	
+	if (q.equals("light grey")) {
+		JOptionPane.showMessageDialog(null, "easy pesy lemon squisy");
+		}
+
+	if (q.equals("dark red")) {
+	JOptionPane.showMessageDialog(null, "avacodo");
+	}
+	
+	if (q.equals("dark blue")) {
+		JOptionPane.showMessageDialog(null, "apple");
+		}
+	
+	if (q.equals("dark green")) {
+		JOptionPane.showMessageDialog(null, "penut butter and waffle");
+		}
+	
+	if (q.equals("dark orange")) {
+		JOptionPane.showMessageDialog(null, "abcdefghijklmnopqrstuvwxyz");
+		}
+	
+	if (q.equals("dark yellow")) {
+		JOptionPane.showMessageDialog(null, "yes");
+		}
+		
+	if (q.equals("dark purple")) {
+		JOptionPane.showMessageDialog(null, "Y U BULLY ME");
+		}
+		
+	if (q.equals("dark pink")) {
+		JOptionPane.showMessageDialog(null, "123456789");
+		}
+	
+	if (q.equals("dark brown")) {
+		JOptionPane.showMessageDialog(null, "hamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburgerhamburger");
+		}
+	
+
+	if (q.equals("dark greblue, light retdy")) {
+		JOptionPane.showMessageDialog(null, "---------------------------");
+		}
+	
+}
+}

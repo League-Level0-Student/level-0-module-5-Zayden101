d1452b1cc5ff713bf5488b58dd6a889e3726b119
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-08-23 12:45:31 -0700

    l

---
+++ src/hhjhjhjh.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class hhjhjhjh {
+public static void main(String[] args) {
+	
+	String q=JOptionPane.showInputDialog("a");
+
+	if (q.equals("no u")) {
+		JOptionPane.showMessageDialog(null,":O");
+		}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "nojonoono"); 
+	}
+	
+
+}
+}

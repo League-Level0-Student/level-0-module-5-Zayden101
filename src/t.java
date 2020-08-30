81792717dd6f495b42c27edb74ad3dc2286b7d49
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-08-30 12:55:41 -0700

    s

---
+++ src/t.java
@@ -0,0 +1,89 @@
+import javax.swing.JOptionPane;
+
+public class t {
+public static void main(String[] args) {
+	for (int i = 0; i < 100; i++) {
+		
+	
+String e=JOptionPane.showInputDialog("on a scale from 1-10, what is your favrit color of the alphabet");
+	
+	if (e.equals(":O")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	String w=JOptionPane.showInputDialog("blue, yellow, or red?");
+	
+	if (w.equals("orange")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	
+	String q=JOptionPane.showInputDialog("do you want a hint?");
+	
+	if (q.equals("no")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	String r=JOptionPane.showInputDialog("table");
+	
+	if (r.equals("chair")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	String t=JOptionPane.showInputDialog("can i have a break");
+	
+	if (t.equals("yes")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	String y=JOptionPane.showInputDialog("what is the password");
+	
+	if (y.equals("wordpass213")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+String u=JOptionPane.showInputDialog("Wiltz Tyler");
+	
+	if (u.equals("Tyler Wiltz")) {
+		JOptionPane.showMessageDialog(null, "ok");
+	}
+	
+	else {
+		JOptionPane.showMessageDialog(null, "no");
+		System.exit(0);
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}

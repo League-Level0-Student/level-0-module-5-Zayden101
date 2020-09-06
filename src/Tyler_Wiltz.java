28889712b7ea77146774b046593c2f702fc6aff3
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-09-06 12:53:03 -0700

    iewgfyhcawugifvdshvilsf hv EPFJVBHAFBSHGBUINATRJGBLAEGHAEIPJRGZEJIjiufnGVFBAIRHEG RHHG jigHV iub gbRHF

---
+++ src/Tyler_Wiltz.java
@@ -0,0 +1,89 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Tyler_Wiltz {
+public static void main(String[] args) {
+	
+	JOptionPane.showMessageDialog(null, "chose a tyler");
+	
+	String q=JOptionPane.showInputDialog("Tyler Wiltz, Plant Wiltz, Movie Wiltz, TV Wiltz, Bike Wiltz, Iphone X Wiltz, Toilet Wiltz, Zoom WIltz, Wiltz Tyler, Small brain Wiltz, Big brain Wiltz, Dark knight Wiltz, Ugly Wiltz, E Wiltz, Old Wiltz, Potato Wiltz, Lonly Wiltz, Break Wiltz, Sponge bob square Wiltz");
+	
+	if (q.equals("Tyler Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you have chosen incorrectly");
+	}
+	
+	if (q.equals("Plant Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you caught on fire and burned");
+	}
+	
+	if (q.equals("Movie Wiltz")) {
+		JOptionPane.showInputDialog("what movie are you watching");
+		JOptionPane.showMessageDialog(null, "I hate that movie");
+	}
+	
+	if (q.equals("TV Wiltz")) {
+		JOptionPane.showMessageDialog(null, "You are a TV that has no battery");
+	}
+	
+	if (q.equals("Bike Wiltz")) {
+		JOptionPane.showMessageDialog(null, "You crash on the grand caynon, fell down mount evrest, and landed in antarcta");
+	}
+	
+	if (q.equals("Iphone X Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you forgot to plug yourself in, you are now dead");
+	}
+	
+	if (q.equals("Toilet Wiltz")) {
+		JOptionPane.showMessageDialog(null, "You flushed your self down");
+	}
+	
+	if (q.equals("Zoom Wiltz")) {
+		JOptionPane.showMessageDialog(null, "You were on a zoom meeting but you internet was so bad your computer exploded");
+	}
+	
+	if (q.equals("Wiltz Tyler")) {
+		JOptionPane.showMessageDialog(null, "incorrectly chosen have you");
+	}
+	
+	if (q.equals("Small brain Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you have an IQ of -278");
+	}
+	
+	if (q.equals("Big brain Wiltz")) {
+		JOptionPane.showMessageDialog(null, "Your brain was so big your head exploded");
+	}
+	
+	if (q.equals("Dark knight Wiltz")) {
+		JOptionPane.showMessageDialog(null, "just because it ends in Wiltz dosent change the fact that i hate the movie");
+	}
+	
+	if (q.equals("E Wiltz")) {
+		JOptionPane.showMessageDialog(null, "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+	}
+	
+	if (q.equals("Ugy Wiltz")) {
+		JOptionPane.showMessageDialog(null, "u ugy");
+	}
+	
+	if (q.equals("Old Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you have fallen and cant get up");
+	}
+	
+	if (q.equals("Potato Wiltz")) {
+		JOptionPane.showMessageDialog(null, "u a potato head");
+	}
+	
+	if (q.equals("Lonly Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you make imagnary friends because you have no friends in real life");
+	}
+	
+	if (q.equals("Break Wiltz")) {
+		JOptionPane.showMessageDialog(null, "You all get 50 year breaks!");
+	}
+	
+	if (q.equals("Sponge bob square Wiltz")) {
+		JOptionPane.showMessageDialog(null, "you ate a crabby patty with patrick inside");
+	}
+}
+}
---
+++ src/delet.java
@@ -0,0 +1,14 @@
+
+public class delet {
+	int num = Integer.parseInt("random");
+
+public static void main(String[] args) {
+	
+	
+	
+	
+	
+	
+	
+}
+}

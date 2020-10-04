6597aab1d321385a3ed62988ef287908eb0d6e7c
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-10-04 12:45:40 -0700

    Create nice.java

---
+++ src/nice.java
@@ -0,0 +1,101 @@
+import javax.swing.JOptionPane;
+
+public class nice {
+public static void main(String[] args) {
+	
+	
+	
+	
+	for (int i = 0; i < 26; i++) {
+	String q=JOptionPane.showInputDialog("a");{
+
+
+	if (q .equals("a")) {
+		JOptionPane.showMessageDialog(null, "no");
+	}
+	if (q .equals("b")) {
+		JOptionPane.showMessageDialog(null,"y");
+	}
+	if (q .equals("c")) {
+		JOptionPane.showMessageDialog(null,"D:");
+	}
+	if (q .equals("d")) {
+		JOptionPane.showMessageDialog(null,"ok");
+	}
+	if (q .equals("e")) {
+		JOptionPane.showMessageDialog(null,"yes");
+	}
+	if (q .equals("f")) {
+		JOptionPane.showMessageDialog(null,":)");
+	}
+	if (q .equals("g")) {
+		JOptionPane.showMessageDialog(null, "low iq");
+	}
+	if (q .equals("h")) {
+		JOptionPane.showMessageDialog(null," ");
+	}
+	if (q .equals("i")) {
+		JOptionPane.showMessageDialog(null,"poop");
+	}
+	if (q .equals("j")) {
+		JOptionPane.showMessageDialog(null,"a");
+	}
+	if (q .equals("k")) {
+		JOptionPane.showMessageDialog(null,",");
+	}
+	if (q .equals("l")) {
+		JOptionPane.showMessageDialog(null,"):(:):(:):(:):(:):(:):(");
+	}
+	if (q .equals("m")) {
+		JOptionPane.showMessageDialog(null, "w");
+	}
+	if (q .equals("n")) {
+		JOptionPane.showMessageDialog(null,"cawfaegag AER");
+	}
+	if (q .equals("o")) {
+		JOptionPane.showMessageDialog(null,":D");
+	}
+	if (q .equals("p")) {
+		JOptionPane.showMessageDialog(null,":P");
+	}
+	if (q .equals("q")) {
+		JOptionPane.showMessageDialog(null,"--=-=-=-=-==-");
+	}
+	if (q .equals("r")) {
+		JOptionPane.showMessageDialog(null,"odcoeot jpoqeirngtviotgiopwrieop");
+	}
+	if (q .equals("s")) {
+		JOptionPane.showMessageDialog(null, "hight iq");
+	}
+	if (q .equals("t")) {
+		JOptionPane.showMessageDialog(null,"[]");
+	}
+	if (q .equals("u")) {
+		JOptionPane.showMessageDialog(null,"poo poo");
+	}
+	if (q .equals("v")) {
+		JOptionPane.showMessageDialog(null,"o");
+	}
+	if (q .equals("w")) {
+		JOptionPane.showMessageDialog(null,"0");
+	}
+	if (q .equals("x")) {
+		JOptionPane.showMessageDialog(null,"O");
+	}
+	if (q .equals("y")) {
+		JOptionPane.showMessageDialog(null,"oeoeoeoeoeoe");
+	}
+	if (q .equals("z")) {
+		JOptionPane.showMessageDialog(null,",.,.,.,.,.,.,.,..,.,.,.,.,..,");
+	}
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	}
+}

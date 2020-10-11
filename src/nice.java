ec54edabe1c20a03c39eadaa1585df966b81d418
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-10-11 12:49:23 -0700

    kvylgyif

--- src/nice.java
+++ src/nice.java
@@ -6,8 +6,8 @@ public static void main(String[] args) {
 	
 	
 	
-	for (int i = 0; i < 26; i++) {
-	String q=JOptionPane.showInputDialog("a");{
+	for (int i = 0; i < 66; i++) {
+	String q=JOptionPane.showInputDialog("ENTER_NUMBER_HERE");{
 
 
 	if (q .equals("a")) {
@@ -87,6 +87,201 @@ public static void main(String[] args) {
 	}
 	if (q .equals("z")) {
 		JOptionPane.showMessageDialog(null,",.,.,.,.,.,.,.,..,.,.,.,.,..,");
+	}
+	if (q .equals("`")) {
+		JOptionPane.showMessageDialog(null, "e");
+	}
+	if (q .equals("~")) {
+		JOptionPane.showMessageDialog(null,"re");
+	}
+	if (q .equals("1")) {
+		JOptionPane.showMessageDialog(null,"eryb");
+	}
+	if (q .equals("!")) {
+		JOptionPane.showMessageDialog(null,"efergabrgt");
+	}
+	if (q .equals("2")) {
+		JOptionPane.showMessageDialog(null,"wawaawawawwa");
+	}
+	if (q .equals("@")) {
+		JOptionPane.showMessageDialog(null,"gt");
+	}
+	if (q .equals("3")) {
+		JOptionPane.showMessageDialog(null, "rtrtrtrt");
+	}
+	if (q .equals("#")) {
+		JOptionPane.showMessageDialog(null,"tertwrtete");
+	}
+	if (q .equals("4")) {
+		JOptionPane.showMessageDialog(null,"wwwwwww");
+	}
+	if (q .equals("$")) {
+		JOptionPane.showMessageDialog(null,"eee");
+	}
+	if (q .equals("5")) {
+		JOptionPane.showMessageDialog(null,"ui");
+	}
+	if (q .equals("%")) {
+		JOptionPane.showMessageDialog(null,"tytytytyt");
+	}
+	if (q .equals("6")) {
+		JOptionPane.showMessageDialog(null, "ioioii");
+	}
+	if (q .equals("^")) {
+		JOptionPane.showMessageDialog(null,"ertrtwertwe4ta");
+	}
+	if (q .equals("7")) {
+		JOptionPane.showMessageDialog(null,"9898989898");
+	}
+	if (q .equals("&")) {
+		JOptionPane.showMessageDialog(null,"=-=-=-=-=-=-=-==-=-=-=--=--=-==-=-=-=-=-=-=-=-=");
+	}
+	if (q .equals("8")) {
+		JOptionPane.showMessageDialog(null, "4545353465625");
+	}
+	if (q .equals("*")) {
+		JOptionPane.showMessageDialog(null,"okkokkookkookk");
+	}
+	if (q .equals("9")) {
+		JOptionPane.showMessageDialog(null, "iq = -45945");
+	}
+	if (q .equals("(")) {
+		JOptionPane.showMessageDialog(null,"{}");
+	}
+	if (q .equals("0")) {
+		JOptionPane.showMessageDialog(null,"[][][[]][][[]");
+	}
+	if (q .equals(")")) {
+		JOptionPane.showMessageDialog(null,"olololololeleoeoeoeoprprpr");
+	}
+	if (q .equals("-")) {
+		JOptionPane.showMessageDialog(null,"][][][][][[]][[[][]][[]");
+	}
+	if (q .equals("_")) {
+		JOptionPane.showMessageDialog(null,"poppopop");
+	}
+	if (q .equals("=")) {
+		JOptionPane.showMessageDialog(null,"ynynynynynyn");
+	}
+	if (q .equals("+")) {
+		JOptionPane.showMessageDialog(null, "uiuuuuuuuuuuuuu");
+	}
+	if (q .equals("[")) {
+		JOptionPane.showMessageDialog(null, "p][o][o]");
+	}
+	if (q .equals("{")) {
+		JOptionPane.showMessageDialog(null,"1212121212");
+	}
+	if (q .equals("]")) {
+		JOptionPane.showMessageDialog(null,"1234567890");
+	}
+	if (q .equals("}")) {
+		JOptionPane.showMessageDialog(null,"m");
+	}
+	if (q .equals("'")) {
+		JOptionPane.showMessageDialog(null,"zazazazazdededed");
+	}
+	if (q .equals(";")) {
+		JOptionPane.showMessageDialog(null,"ryryryryr");
+	}
+	if (q .equals(":")) {
+		JOptionPane.showMessageDialog(null, "bbbbbbbbbbb");
+	}
+	if (q .equals(",")) {
+		JOptionPane.showMessageDialog(null,"iiiiiiiiiii");
+	}
+	if (q .equals(".")) {
+		JOptionPane.showMessageDialog(null,"po op");
+	}
+	if (q .equals("/")) {
+		JOptionPane.showMessageDialog(null,"l");
+	}
+	if (q .equals("?")) {
+		JOptionPane.showMessageDialog(null,";''';;'");
+	}
+	if (q .equals(">")) {
+		JOptionPane.showMessageDialog(null, "deeddeded");
+	}
+	if (q .equals("<")) {
+		JOptionPane.showMessageDialog(null, "////'/''/'/'/'");
+	}
+	if (q .equals("A")) {
+		JOptionPane.showMessageDialog(null, "NO");
+	}
+	if (q .equals("B")) {
+		JOptionPane.showMessageDialog(null,"Y");
+	}
+	if (q .equals("C")) {
+		JOptionPane.showMessageDialog(null,"d;");
+	}
+	if (q .equals("D")) {
+		JOptionPane.showMessageDialog(null,"OK");
+	}
+	if (q .equals("E")) {
+		JOptionPane.showMessageDialog(null,"YES");
+	}
+	if (q .equals("F")) {
+		JOptionPane.showMessageDialog(null,";0");
+	}
+	if (q .equals("G")) {
+		JOptionPane.showMessageDialog(null, "LOW IQ");
+	}
+	if (q .equals("H")) {
+		JOptionPane.showMessageDialog(null," ");
+	}
+	if (q .equals("I")) {
+		JOptionPane.showMessageDialog(null,"POOP");
+	}
+	if (q .equals("J")) {
+		JOptionPane.showMessageDialog(null,"A");
+	}
+	if (q .equals("K")) {
+		JOptionPane.showMessageDialog(null,"<");
+	}
+	if (q .equals("L")) {
+		JOptionPane.showMessageDialog(null,"0;9;0;9;0;9;0;9;0;9;0;");
+	}
+	if (q .equals("M")) {
+		JOptionPane.showMessageDialog(null, "W");
+	}
+	if (q .equals("N")) {
+		JOptionPane.showMessageDialog(null,"CAWFAEGAG aer");
+	}
+	if (q .equals("O")) {
+		JOptionPane.showMessageDialog(null,";d");
+	}
+	if (q .equals("P")) {
+		JOptionPane.showMessageDialog(null,";p");
+	}
+	if (q .equals("Q")) {
+		JOptionPane.showMessageDialog(null,"__+_+_+_++__+");
+	}
+	if (q .equals("R")) {
+		JOptionPane.showMessageDialog(null,"ODCOEOT JPOQEIRNGTVIOTGIOPWEREOP");
+	}
+	if (q .equals("S")) {
+		JOptionPane.showMessageDialog(null, "HIGHT IQ");
+	}
+	if (q .equals("T")) {
+		JOptionPane.showMessageDialog(null,"}{");
+	}
+	if (q .equals("U")) {
+		JOptionPane.showMessageDialog(null,"POO POO");
+	}
+	if (q .equals("V")) {
+		JOptionPane.showMessageDialog(null,"r");
+	}
+	if (q .equals("W")) {
+		JOptionPane.showMessageDialog(null,"vf");
+	}
+	if (q .equals("X")) {
+		JOptionPane.showMessageDialog(null,"epeppep");
+	}
+	if (q .equals("Y")) {
+		JOptionPane.showMessageDialog(null,"OEOEOEOEOEEO");
+	}
+	if (q .equals("Z")) {
+		JOptionPane.showMessageDialog(null,"<><><><><><>>>><");
 	}
 	}
 	}

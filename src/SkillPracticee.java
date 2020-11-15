4ab5834b90f5b7cfab61099d22ad142cae9e0579
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2020-11-15 12:49:11 -0800

    o

--- src/SkillPracticee.java
+++ src/SkillPracticee.java
@@ -1,6 +1,4 @@
-import javax.swing.JOptionPane;
-
-public class SkillPractice {
+public class SkillPracticee {
 public static void main(String[] args) {
 
 SkillPractice skills = new SkillPractice();
@@ -14,7 +12,7 @@ SkillPractice skills = new SkillPractice();
 void skill1() {
 // Use pop-ups for the following.
 // Ask the user how many dimes they have
-JOptionPane.showInputDialog("how many dimes do you have.");
+
 
 
 
@@ -24,20 +22,15 @@ JOptionPane.showInputDialog("how many dimes do you have.");
 
 
 // Ask the user how tall they are (inches)
-String u=JOptionPane.showInputDialog("How tall are you.");
+
 
 
 
 // If they are shorter than 36 inches, tell them to eat their Wheaties
 
 
-if (u.equals("37")){
-	JOptionPane.showMessageDialog(null, "e");
-}
 
-else {
-	JOptionPane.showMessageDialog(null, "o");
-}
+
 }
 
 void skill2() { // Write a loop to print every third number between 1 and 30 to the console
@@ -66,20 +59,15 @@ void skill3() { // Get a random number that is less than 20 and print it to the 
 
 void skill4() { // In a pop-up, ask the user for the city they live in
 
-	String e=JOptionPane.showInputDialog("what city do u live in");
+
 
 // If they answered "San Diego", tell them they live in America's Finest City
 
- if (e.equals("San diego")){
-	JOptionPane.showMessageDialog(null, "wallrus");
-}
 
 
 // Otherwise, tell them to move to San Diego
 
- else {
-		JOptionPane.showMessageDialog(null, "toilet");
-	}
+
 
 // Create a variable - cars - and initialize it to the number of cars your family has. // If there are 0 cars, use a pop-up to display, "I bet you use public transportation."
 
@@ -90,19 +78,18 @@ void skill4() { // In a pop-up, ask the user for the city they live in
 
 
 // If there is more than 1 car, use a pop-up to display how many wheels the // cars have between them.
- 
+
 
 
 }
 
 void skill5() { // In a pop-up, ask the user for the name of their school
 
-	JOptionPane.showInputDialog("what school do u go to");
+
 
 // In another pop-up, tell the user, that their school is a fantastic school. // You must include the name of the school in the message.
 
-	JOptionPane.showMessageDialog(null, "Mc Donald dipers");
+
 
 }
 }
-

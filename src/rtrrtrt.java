9b01a64753f85dcf9ca622f8db2a78c433bc98c5
Zayden101 <52078609+Zayden101@users.noreply.github.com>
2021-01-10 12:57:21 -0800

    w

--- src/rtrrtrt.java
+++ src/rtrrtrt.java
@@ -131,12 +131,171 @@ public static void main(String[] args) {
 		b.setRandomPenColor();
 	a.turn(10);
 	b.turn(10);
+	a.move(5);
+	b.move(5);
+	}
+	a.move(10);
+	b.move(10);
+
+	a.turn(180);
+	b.turn(180);
+	a.move(300);
+	b.move(300);
+	for (int u = 0; u < 40; u++) {
+		a.setRandomPenColor();
+		b.setRandomPenColor();
+	a.turn(10);
+	b.turn(10);
 	a.move(10);
 	b.move(10);
 	}
 	a.move(10);
 	b.move(10);
+	for (int u = 0; u < 40; u++) {
+		a.setRandomPenColor();
+		b.setRandomPenColor();
+	a.turn(10);
+	b.turn(10);
+	a.move(10);
+	b.move(10);
+	}
+	a.move(10);
+	b.move(10);
+	for (int u = 0; u < 40; u++) {
+		a.setRandomPenColor();
+		b.setRandomPenColor();
+	a.turn(10);
+	b.turn(10);
+	a.move(5);
+	b.move(5);
+	}
+	a.move(10);
+	b.move(10);
 
+	for (int u = 0; u < 40; u++) {
+		a.setRandomPenColor();
+		b.setRandomPenColor();
+	a.turn(10);
+	b.turn(10);
+	a.move(10);
+	b.move(10);
+	}
 	
+	for (int i = 0; i < 65; i++) {
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.turn(5);
+		b.turn(5);
+	}
+	a.turn(120);
+	b.turn(120);
+	a.move(100);
+	b.move(100);
+	for (int i = 0; i < 65; i++) {
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.turn(5);
+		b.turn(5);
+	}
+	a.turn(120);
+	b.turn(120);
+	a.move(100);
+	b.move(100);
+	for (int i = 0; i < 65; i++) {
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.move(100);
+		b.move(100);
+		a.turn(120);
+		b.turn(120);
+		a.turn(5);
+		b.turn(5);
+	}
+	Robot c = new Robot(); 
+	Robot d = new Robot();
+	c.setSpeed(99999);
+	c.miniaturize();
+	c.penDown();
+	d.setSpeed(99999);
+	d.miniaturize();
+	d.penDown();
+	d.turn(180);
+	c.move(100);
+	d.move(100);
+	c.setRandomPenColor();
+	d.setRandomPenColor();
+	for (int i = 0; i < 65; i++) {
+		c.move(100);
+		d.move(100);
+		c.turn(120);
+		d.turn(120);
+		c.move(100);
+		d.move(100);
+		c.turn(120);
+		d.turn(120);
+		c.move(100);
+		d.move(100);
+		c.turn(120);
+		d.turn(120);
+		c.turn(5);
+		d.turn(5);
+	}
+	
+	Robot e = new Robot(); 
+	Robot f = new Robot();
+	e.setSpeed(99999);
+	e.miniaturize();
+	e.penDown();
+	f.setSpeed(99999);
+	f.miniaturize();
+	f.penDown();
+	f.turn(90);
+	e.turn(-90);
+	e.move(100);
+	f.move(100);
+	e.setRandomPenColor();
+	f.setRandomPenColor();
+	for (int i = 0; i < 65; i++) {
+		e.move(100);
+		f.move(100);
+		e.turn(120);
+		f.turn(120);
+		e.move(100);
+		f.move(100);
+		e.turn(120);
+		f.turn(120);
+		e.move(100);
+		f.move(100);
+		e.turn(120);
+		f.turn(120);
+		e.turn(5);
+		f.turn(5);
+	}
 }
 }
